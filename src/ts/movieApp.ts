a60e29773df78f37060da32099aed5bc852bebc6
Anna <anna.viklund1@medieinstitutet.se>
2025-09-16 14:53:01 +0200

    Moved html-functions to separate file

--- src/ts/movieApp.ts
+++ src/ts/movieApp.ts
@@ -1,5 +1,6 @@
 import { IMovie } from "./models/Movie";
 import { getData } from "./services/movieService";
+import { renderMovies, displayNoResult } from "./ui/renderHTML";
 
 let movies: IMovie[] = [];
 
@@ -12,19 +13,18 @@ export const init = () => {
 };
 
 export async function handleSubmit() {
-  let searchText = (document.getElementById("searchText") as HTMLInputElement)
-    .value;
+  const input = (document.getElementById("searchText") as HTMLInputElement);
 
-  let container: HTMLDivElement = document.getElementById(
+  const container = document.getElementById(
     "movie-container"
   ) as HTMLDivElement;
   container.innerHTML = "";
 
   try {
-    movies = await getData(searchText);
+    movies = await getData(input.value);
 
     if (movies.length > 0) {
-      createHtml(movies, container);
+      renderMovies(container, movies);
     } else {
       displayNoResult(container);
     }
@@ -32,29 +32,3 @@ export async function handleSubmit() {
     displayNoResult(container);
   }
 }
-
-export const createHtml = (movies: IMovie[], container: HTMLDivElement) => {
-  for (let i = 0; i < movies.length; i++) {
-    let movie = document.createElement("div");
-    let title = document.createElement("h3");
-    let img = document.createElement("img");
-
-    movie.classList.add("movie");
-    title.innerHTML = movies[i].Title;
-    img.src = movies[i].Poster;
-    img.alt = movies[i].Title;
-
-    movie.appendChild(title);
-    movie.appendChild(img);
-
-    container.appendChild(movie);
-  }
-};
-
-export const displayNoResult = (container: HTMLDivElement) => {
-  let noMessage = document.createElement("p");
-
-  noMessage.innerHTML = "Inga sökresultat att visa";
-
-  container.appendChild(noMessage);
-};
